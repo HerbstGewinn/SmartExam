49074f35258659c63204fbc745115dcf21541c9c
HerbstGewinn <103055424+HerbstGewinn@users.noreply.github.com>
2024-10-22 20:48:32 +0200

    Update BEST_PDF_STUDY_APP.py

--- BEST_PDF_STUDY_APP.py
+++ BEST_PDF_STUDY_APP.py
@@ -20,12 +20,38 @@ from fpdf import FPDF
 import base64
 
 __version__ = "1.1.0"
+
 hide_streamlit_style = """
-            <style>
-            [data-testid="stToolbar"] {visibility: hidden !important;}
-            footer {visibility: hidden !important;}
-            </style>
-            """
+                <style>
+                div[data-testid="stToolbar"] {
+                visibility: hidden;
+                height: 0%;
+                position: fixed;
+                }
+                div[data-testid="stDecoration"] {
+                visibility: hidden;
+                height: 0%;
+                position: fixed;
+                }
+                div[data-testid="stStatusWidget"] {
+                visibility: hidden;
+                height: 0%;
+                position: fixed;
+                }
+                #MainMenu {
+                visibility: hidden;
+                height: 0%;
+                }
+                header {
+                visibility: hidden;
+                height: 0%;
+                }
+                footer {
+                visibility: hidden;
+                height: 0%;
+                }
+                </style>
+                """
 st.markdown(hide_streamlit_style, unsafe_allow_html=True)
 
 
