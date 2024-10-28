4ec6dfa1df9bce312d84f2752caf8aa5031c2434
HerbstGewinn <103055424+HerbstGewinn@users.noreply.github.com>
2024-10-28 12:53:41 +0100

    Update 💵Pricing.py

--- pages/💵Pricing.py
+++ pages/💵Pricing.py
@@ -55,12 +55,13 @@ def handle_checkout(plan_id):
             # Display the clickable link for manual redirection
             #st.markdown(f'[Click here to proceed to payment]({checkout_url})')
 
-                redirect_script = f"""
+            # JavaScript for automatic redirection without a clickable link
+            redirect_script = f"""
             <script type="text/javascript">
                 window.location.href = "{checkout_url}";
             </script>
-        """
-        st.markdown(redirect_script, unsafe_allow_html=True)
+            """
+            st.markdown(redirect_script, unsafe_allow_html=True)
             
             # Optionally: Automatically redirect to Stripe checkout after some time
             #st.markdown(f"""
