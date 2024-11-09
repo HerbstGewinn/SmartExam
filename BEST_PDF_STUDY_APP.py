c70262bf2438908a191624105991a9aac03a84f4
HerbstGewinn <103055424+HerbstGewinn@users.noreply.github.com>
2024-11-09 02:05:08 +0100

    Update BEST_PDF_STUDY_APP.py
    Only 2 MC Exams

--- BEST_PDF_STUDY_APP.py
+++ BEST_PDF_STUDY_APP.py
@@ -270,8 +270,8 @@ def main():
     # --- Check if the user has reached the usage limit ---
     # Only enforce usage limit if the subscription tier is "FREE"
     if subscription_tier == "FREE":
-        # Check if the mc_upload_count is greater than or equal to 5
-        if mc_upload_count and mc_upload_count >= 10:
+        # Check if the mc_upload_count is greater than or equal to 2
+        if mc_upload_count and mc_upload_count >= 2:
             st.error("You have reached your free usage limit. Upgrade to a higher version for an advanced study progress.")
 
             # Display the "Upgrade Now" button only when the limit is exceeded
