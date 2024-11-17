5d303ec673bad9ec40632b7dae248de03e771119
HerbstGewinn <103055424+HerbstGewinn@users.noreply.github.com>
2024-11-17 16:40:50 +0100

    Update BEST_PDF_STUDY_APP.py
    try

--- BEST_PDF_STUDY_APP.py
+++ BEST_PDF_STUDY_APP.py
@@ -283,7 +283,7 @@ def main():
                 """, unsafe_allow_html=True)
 
             st.stop()  # Stop further interaction if the limit is reached #return changed to st.stop
-       else:
+          else:
             st.info("Welcome, FREE user! You still have uploads available. Enjoy the features.")       #Added to allow access to free users with less than 3 uploads
     elif subscription_tier in ["PREMIUM", "PRO"]:
         st.success("Welcome, PREMIUM/PRO user! You have unlimited access to all features.")       #Added for handling the Premium/pro users 
